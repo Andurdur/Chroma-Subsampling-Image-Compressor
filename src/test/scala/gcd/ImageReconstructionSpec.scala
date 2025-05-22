068cae69e048919804bfd2d6fe6e9f6f96e97b8e
EthanKCesario <116698330+EthanKCesario@users.noreply.github.com>
2025-05-22 16:29:51 -0700

    Update ImageReconstructionSpec.scala

--- src/test/scala/gcd/ImageReconstructionSpec.scala
+++ src/test/scala/gcd/ImageReconstructionSpec.scala
@@ -8,19 +8,34 @@ class ImageReconstructionSpec extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "ImageCompressorTop"
 
   it should "dump YCbCr to ycbcr_out.bin" in {
-    test(new ImageCompressorTop(512, 512, /*subMode=*/2, /*downFactor=*/2)) { dut =>
+    test(new ImageCompressorTop(512, 512, subMode = 2, downFactor = 2)) { dut =>
       val fos = new java.io.FileOutputStream("ycbcr_out.bin")
-      val totalPixels = (512/2) * (512/2)
-      var seen = 0
-      while (seen < totalPixels) {
-        dut.clock.step()
+      val W = 512; val H = 512
+      val totalOut = (W/2) * (H/2)
+      var inCount = 0
+      var outCount = 0
+
+      dut.io.in.valid.poke(false.B)
+
+      while (outCount < totalOut) {
+        if (inCount < W * H && dut.io.in.ready.peek().litToBoolean) {
+          dut.io.in.bits.r.poke(0.U)
+          dut.io.in.bits.g.poke(0.U)
+          dut.io.in.bits.b.poke(0.U)
+          dut.io.in.valid.poke(true.B)
+          inCount += 1
+        } else {
+          dut.io.in.valid.poke(false.B)
+        }
+        
         if (dut.io.out.valid.peek().litToBoolean) {
           val y  = dut.io.out.bits.y.peek().litValue.toByte
           val cb = dut.io.out.bits.cb.peek().litValue.toByte
           val cr = dut.io.out.bits.cr.peek().litValue.toByte
           fos.write(Array(y, cb, cr))
-          seen += 1
+          outCount += 1
         }
+        dut.clock.step()
       }
       fos.close()
     }
