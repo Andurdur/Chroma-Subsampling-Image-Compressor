2e6a535896f7af9537429ff88691bba6ba5baec0
EthanKCesario <116698330+EthanKCesario@users.noreply.github.com>
2025-05-22 15:26:10 -0700

    Update ChromaSubsampler.scala

--- src/main/scala/gcd/ChromaSubsampler.scala
+++ src/main/scala/gcd/ChromaSubsampler.scala
@@ -3,10 +3,11 @@ package jpeg
 import chisel3._
 import chisel3.util._
 import PixelBundles._
+
 /**
  * ChromaSubsampler supports 4:4:4, 4:2:2, 4:2:0, and 4:1:1 subsampling.
- * @param width   image width in pixels (must be even for horizontal subsampling)
- * @param height  image height in pixels (must be even for vertical subsampling where required)
+ * @param width   image width (must be even for horizontal subsampling)
+ * @param height  image height (must be even for vertical subsampling)
  * @param mode    0->4:4:4, 1->4:2:2, 2->4:2:0, 3->4:1:1
  */
 class ChromaSubsampler(width: Int, height: Int, mode: Int) extends Module {
@@ -32,37 +33,50 @@ class ChromaSubsampler(width: Int, height: Int, mode: Int) extends Module {
     rowCnt := 0.U
   }
 
-  def sampleH: Bool = mode match {
-    case 0 => true.B                       
-    case 1 => (colCnt(0) === 0.U)         
-    case 2 => (colCnt(0) === 0.U)         
-    case 3 => (colCnt(1,0) === 0.U)        
+  val sampleH = mode match {
+    case 0 => true.B
+    case 1 => (colCnt(0) === 0.U)
+    case 2 => (colCnt(0) === 0.U)
+    case 3 => (colCnt(1,0) === 0.U)
   }
-  def sampleV: Bool = mode match {
-    case 0 => true.B                        
-    case 1 => true.B                       
-    case 2 => (rowCnt(0) === 0.U)          
-    case 3 => true.B                        
+  val sampleV = mode match {
+    case 0 => true.B
+    case 1 => true.B
+    case 2 => (rowCnt(0) === 0.U)
+    case 3 => true.B
   }
-  val doSub = sampleH && sampleV
+  val doSample = sampleH && sampleV && io.in.valid
+
   val cbBuf0 = SyncReadMem(width, UInt(8.W))
   val cbBuf1 = SyncReadMem(width, UInt(8.W))
   val crBuf0 = SyncReadMem(width, UInt(8.W))
   val crBuf1 = SyncReadMem(width, UInt(8.W))
-  val writeSel = RegInit(false.B)
-  when(io.eol && io.in.fire()) { writeSel := ~writeSel }
 
-  when(io.in.fire()) {
-    val cbW = io.in.bits.cb
-    val crW = io.in.bits.cr
-    Mux(writeSel, cbBuf1, cbBuf0).write(colCnt, cbW)
-    Mux(writeSel, crBuf1, crBuf0).write(colCnt, crW)
+  val writeSel = RegInit(false.B)
+  when(io.in.fire() && io.eol) {
+    writeSel := ~writeSel
   }
 
-  val cbRead = Mux(writeSel, cbBuf0, cbBuf1) 
-  val crRead = Mux(writeSel, crBuf0, crBuf1)
-  val cbShift = Reg(Vec(2, UInt(8.W)))
-  val crShift = Reg(Vec(2, UInt(8.W)))
+  when(io.in.fire()) {
+    when(writeSel) {
+      cbBuf1.write(colCnt, io.in.bits.cb)
+      crBuf1.write(colCnt, io.in.bits.cr)
+    }.otherwise {
+      cbBuf0.write(colCnt, io.in.bits.cb)
+      crBuf0.write(colCnt, io.in.bits.cr)
+    }
+  }
+
+  val cbPrevBuf = Mux(writeSel, cbBuf0, cbBuf1)
+  val crPrevBuf = Mux(writeSel, crBuf0, crBuf1)
+
+  val cbUp1 = cbPrevBuf.read(colCnt)
+  val cbUp0 = cbPrevBuf.read(colCnt - 1.U)
+  val crUp1 = crPrevBuf.read(colCnt)
+  val crUp0 = crPrevBuf.read(colCnt - 1.U)
+
+  val cbShift = RegInit(VecInit(Seq.fill(2)(0.U(8.W))))
+  val crShift = RegInit(VecInit(Seq.fill(2)(0.U(8.W))))
   when(io.in.fire()) {
     cbShift(0) := cbShift(1)
     cbShift(1) := io.in.bits.cb
@@ -72,37 +86,37 @@ class ChromaSubsampler(width: Int, height: Int, mode: Int) extends Module {
 
   val cbSub = Wire(UInt(8.W))
   val crSub = Wire(UInt(8.W))
-  when(doSub) {
-    mode match {
-      case 0 => 
+  when(doSample) {
+    switch(mode.U) {
+      is(0.U) {
         cbSub := io.in.bits.cb
         crSub := io.in.bits.cr
-      case 1 => 
-        cbSub := (cbShift(1).asUInt + cbShift(0).asUInt + 1.U) >> 1
-        crSub := (crShift(1).asUInt + crShift(0).asUInt + 1.U) >> 1
-      case 2 => 
-        val cbUp1 = cbBuf1.read(colCnt)
-        val cbUp0 = cbBuf1.read(colCnt - 1.U)
-        val crUp1 = crBuf1.read(colCnt)
-        val crUp0 = crBuf1.read(colCnt - 1.U)
-        val sumCb = cbShift(1).asUInt + cbShift(0).asUInt + cbUp1 + cbUp0
-        val sumCr = crShift(1).asUInt + crShift(0).asUInt + crUp1 + crUp0
+      }
+      is(1.U) { 
+        cbSub := (cbShift(1) + cbShift(0) + 1.U) >> 1
+        crSub := (crShift(1) + crShift(0) + 1.U) >> 1
+      }
+      is(2.U) { 
+        val sumCb = cbShift(0) + cbShift(1) + cbUp0 + cbUp1
+        val sumCr = crShift(0) + crShift(1) + crUp0 + crUp1
         cbSub := (sumCb + 2.U) >> 2
         crSub := (sumCr + 2.U) >> 2
-      case 3 => 
-        val cbSum = cbShift.foldLeft(0.U)((acc, x) => acc + x) 
-        val crSum = crShift.foldLeft(0.U)((acc, x) => acc + x)
-        cbSub := (cbSum + 1.U) >> 1 
-        crSub := (crSum + 1.U) >> 1
+      }
+      is(3.U) { 
+        val sumCb = cbShift(0) + cbShift(1)
+        val sumCr = crShift(0) + crShift(1)
+        cbSub := (sumCb + 1.U) >> 1
+        crSub := (sumCr + 1.U) >> 1
+      }
     }
   }.otherwise {
     cbSub := 0.U
     crSub := 0.U
   }
 
-  io.out.valid := io.in.valid && doSub
+  io.out.valid := doSample
   io.in.ready  := io.out.ready
-
+  io.out.bits := Mux(io.out.valid, io.in.bits.asInstanceOf[new PixelYCbCr].cloneType, Wire(new PixelYCbCr))
   io.out.bits.y  := io.in.bits.y
   io.out.bits.cb := cbSub
   io.out.bits.cr := crSub
