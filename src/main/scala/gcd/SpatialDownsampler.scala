bb1f7b4ceea3dffc552d5f6f92c7d2bc696a33b4
EthanKCesario <116698330+EthanKCesario@users.noreply.github.com>
2025-05-22 15:35:46 -0700

    Create SpatialDownsampler.scala

---
+++ src/main/scala/gcd/SpatialDownsampler.scala
@@ -0,0 +1,49 @@
+package jpeg
+
+import chisel3._
+import chisel3.util._
+import PixelBundles._
+
+class SpatialDownsampler(width: Int, height: Int, factor: Int) extends Module {
+  require(width > 0 && height > 0)
+  require(factor == 2 || factor == 4 || factor == 8)
+
+  val io = IO(new Bundle {
+    val in  = Flipped(Decoupled(new PixelYCbCr))
+    val out = Decoupled(new PixelYCbCr)
+    val sof = Input(Bool())
+    val eol = Input(Bool())
+  })
+
+  val colCnt = RegInit(0.U(log2Ceil(width).W))
+  val rowCnt = RegInit(0.U(log2Ceil(height).W))
+
+  when(io.in.fire) {
+    when(io.eol) {
+      colCnt := 0.U
+      rowCnt := rowCnt + 1.U
+    }.otherwise {
+      colCnt := colCnt + 1.U
+    }
+  }
+  when(io.sof) { rowCnt := 0.U }
+
+  val sampleH = factor match {
+    case 2 => colCnt(0) === 0.U
+    case 4 => colCnt(1,0) === 0.U
+    case 8 => colCnt(2,0) === 0.U
+  }
+  val sampleV = factor match {
+    case 2 => rowCnt(0) === 0.U
+    case 4 => rowCnt(1,0) === 0.U
+    case 8 => rowCnt(2,0) === 0.U
+  }
+  val doSample = io.in.valid && sampleH && sampleV
+
+  io.out.valid := doSample
+  io.in.ready  := io.out.ready
+
+  io.out.bits.y  := io.in.bits.y
+  io.out.bits.cb := io.in.bits.cb
+  io.out.bits.cr := io.in.bits.cr
+}
