ab154f4db3391fc0dd499b9f1466d6a9f314471e
EthanKCesario <116698330+EthanKCesario@users.noreply.github.com>
2025-05-23 16:46:21 -0700

    Update SpatialDownsamplerSpec.scala

--- src/test/scala/jpeg/SpatialDownsamplerSpec.scala
+++ src/test/scala/jpeg/SpatialDownsamplerSpec.scala
@@ -1,52 +1,41 @@
 package jpeg
 
 import chisel3._
-import chisel3.tester._
 import chisel3.util._
+import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import PixelBundle._
 
 class SpatialDownsamplerSpec extends AnyFlatSpec with ChiselScalatestTester with Matchers {
   "SpatialDownsampler" should "downsample a 4x4 YCbCr image by factor 2" in {
     test(new SpatialDownsampler(4, 4, 2)) { dut =>
-      // Generate a 4x4 identity pattern: pixel value = row*4 + col
-      val inPixels = (0 until 16).map(i => {
-        val y  = (i & 0xFF).U
-        val cb = (128 + i).U  // arbitrary offset
-        val cr = (64 + i).U   // arbitrary offset
-        (y, cb, cr)
-      })
+      val inPixels = (0 until 16).map(i => (i.U(8.W), (128 + i).U(8.W), (64 + i).U(8.W)))
+      dut.io.out.ready.poke(true.B)
+      var inCount = 0
+      var outCount = 0
 
-      // Drive inputs
-      fork {
-        for ((y, cb, cr) <- inPixels) {
+      while (outCount < 4) {
+        if (inCount < inPixels.size && dut.io.in.ready.peek().litToBoolean) {
+          val (y, cb, cr) = inPixels(inCount)
           dut.io.in.bits.y.poke(y)
           dut.io.in.bits.cb.poke(cb)
           dut.io.in.bits.cr.poke(cr)
           dut.io.in.valid.poke(true.B)
-          dut.clock.step()
+          inCount += 1
+        } else {
+          dut.io.in.valid.poke(false.B)
         }
-        dut.io.in.valid.poke(false.B)
-      }.fork {
-        // Read downsampled outputs: expect 4 outputs (every 2nd pixel on 2x2 grid)
-        val got = scala.collection.mutable.ArrayBuffer[(Int,Int,Int)]()
-        while (got.size < 4) {
-          if (dut.io.out.valid.peek().litToBoolean) {
-            got += ((dut.io.out.bits.y.peek().litValue.toInt,
-                     dut.io.out.bits.cb.peek().litValue.toInt,
-                     dut.io.out.bits.cr.peek().litValue.toInt))
-            dut.io.out.ready.poke(true.B)
-          }
-          dut.clock.step()
+
+        if (dut.io.out.valid.peek().litToBoolean) {
+          val (yExp, cbExp, crExp) = inPixels((outCount * 2) * 4 + (outCount * 2))
+          dut.io.out.bits.y.peek().litValue.toInt shouldBe yExp.litValue.toInt
+          dut.io.out.bits.cb.peek().litValue.toInt shouldBe cbExp.litValue.toInt
+          dut.io.out.bits.cr.peek().litValue.toInt shouldBe crExp.litValue.toInt
+          outCount += 1
         }
-        // Check expected positions: (0,0), (0,2), (2,0), (2,2)
-        val expIndices = Seq(0, 2, 8, 10)
-        got.zip(expIndices).foreach { case ((y,cb,cr), idx) =>
-          y   shouldBe idx
-          cb  shouldBe (128 + idx)
-          cr  shouldBe (64 + idx)
-        }
-      }.join()
+        dut.clock.step()
+      }
     }
   }
 }
