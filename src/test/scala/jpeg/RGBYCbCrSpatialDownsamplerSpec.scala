e97014fd6eab9d518dde7b7c81b0e80c4c41f945
EthanKCesario <116698330+EthanKCesario@users.noreply.github.com>
2025-05-23 16:10:23 -0700

    Update RGBYCbCrSpatialDownsamplerSpec.scala

--- src/test/scala/jpeg/RGBYCbCrSpatialDownsamplerSpec.scala
+++ src/test/scala/jpeg/RGBYCbCrSpatialDownsamplerSpec.scala
@@ -1,8 +1,10 @@
 package jpeg
 
 import chisel3._
+import chisel3.util._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
+import PixelBundles._
 
 class RGBYCbCrSpatialDownsamplerSpec extends AnyFlatSpec with ChiselScalatestTester {
   "RGB2YCbCr + SpatialDownsampler" should "convert RGB=0 to YCbCr and downsample" in {
@@ -12,28 +14,23 @@ class RGBYCbCrSpatialDownsamplerSpec extends AnyFlatSpec with ChiselScalatestTes
         val out = Decoupled(new PixelYCbCr)
       })
       val conv = Module(new RGB2YCbCr)
-      val spat = Module(new SpatialDownsampler(width = 4, height = 4, factor = 2))
-      // wire-up
-      io.in  <> conv.io.in
+      val spat = Module(new SpatialDownsampler(4, 4, 2))
+      io.in <> conv.io.in
       conv.io.out <> spat.io.in
       spat.io.sof := false.B
       spat.io.eol := false.B
       io.out <> spat.io.out
     }) { dut =>
-      // Prepare counters
       val totalPixels = 16
       val expectedOutputs = 4
       var inCount = 0
       var outCount = 0
 
-      // Initialize ready/valid
       dut.io.out.ready.poke(true.B)
       dut.io.in.valid.poke(false.B)
 
-      // Drive pixels and collect outputs
       while (outCount < expectedOutputs) {
         if (inCount < totalPixels && dut.io.in.ready.peek().litToBoolean) {
-          // RGB zero -> Y=0, Cb=128, Cr=128
           dut.io.in.bits.r.poke(0.U)
           dut.io.in.bits.g.poke(0.U)
           dut.io.in.bits.b.poke(0.U)
